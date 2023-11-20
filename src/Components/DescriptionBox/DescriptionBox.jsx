759addc5d380ade233b48bf1e88b49a8e15ad946
muratkistan <muratkistan@gmail.com>
2023-11-20 22:31:48 +0300

    full project

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionBoxNavigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio ea
+          voluptatum facilis quidem earum quia tempore aperiam consectetur nihil
+          inventore, tenetur vitae nulla veniam ab, totam unde expedita animi
+          modi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+          voluptate nam error, distinctio repellendus eum obcaecati odit?
+          Provident minima quasi rem molestias quia architecto cumque! Nam culpa
+          placeat nisi fugit.
+        </p>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, laudantium rerum incidunt, iure quisquam enim perferendis adipisci soluta dolorem provident recusandae! Doloribus iure nihil odio deserunt mollitia ad adipisci atque?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
